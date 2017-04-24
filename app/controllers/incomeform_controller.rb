6c63f21ce50fc3581d940c49a6117820a3be0f48
SHIBOUKAN <tomoki.sugimoto.rits@gmail.com>
2017-04-24 08:19:47 +0000

    ratio patch

--- app/controllers/incomeform_controller.rb
+++ app/controllers/incomeform_controller.rb
@@ -1,15 +1,17 @@
 class IncomeformController < ApplicationController
   
   def new
+  ratio = params[:ratio]
   kihon = params[:kihon]
   kinmu = params[:kinmu]
   total = nil
     if kihon.present? && kinmu.present?
       if  kinmu.to_f<0.5
-        total=kihon.to_i*0.75*2.9*kinmu.to_f*2
+        total=kihon.to_i*0.75*2.9*kinmu.to_f*2*ratio.to_f*0.01
         
       else
-        total=kihon.to_i*0.75*2.9
+        total=kihon.to_i*0.75*2.9*ratio.to_f*0.01
+        
       end
       session[:total] = total
     redirect_to new_item_path

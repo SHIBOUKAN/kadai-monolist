c4d8bc8fdc417e006745b2882d0b4b960c3e4d82
SHIBOUKAN <tomoki.sugimoto.rits@gmail.com>
2017-04-24 02:34:17 +0000

    have/want fac fixs

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,5 @@ class Item < ApplicationRecord
   has_many :wants
   has_many :want_users, through: :wants, class_name: 'User', source: :user
   has_many :haves
-  has_many :have_users, through: :haves, class_name: 'Item', source: :user
+  has_many :have_users, through: :haves, class_name: 'User', source: :user
 end
